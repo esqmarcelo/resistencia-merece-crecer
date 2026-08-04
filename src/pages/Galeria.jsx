2f73a35555030663271cbccfc6f92121136b8c4d
esqmarcelo <esquivelgomezmarcelo@gmail.com>
2026-08-04 18:36:33 -0300

    se agrego secciones vacias en galeria para agregar imagenes

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,4 +1,16 @@
 function Galeria() {
-  return <h1>Galería</h1>
+  const fotos = Array.from({ length: 9 })
+
+  return (
+    <section className="container galeria-section">
+      <h1>Galería</h1>
+      <div className="galeria-grid">
+        {fotos.map((_, index) => (
+          <div className="galeria-item" key={index}></div>
+        ))}
+      </div>
+    </section>
+  )
 }
+
 export default Galeria
