c9365e5ad62726030ee7e11f77b23b826a580be9
esqmarcelo <esquivelgomezmarcelo@gmail.com>
2026-08-04 20:21:27 -0300

    cambios en galeria

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,11 +1,19 @@
 function Galeria() {
-  const fotos = Array.from({ length: 9 })
+  const imagenes = Array.from({ length: 9 })
+  const videos = Array.from({ length: 9 })
 
   return (
     <section className="container galeria-section">
-      <h1>Galería</h1>
+      <h2 className="galeria-subtitulo">Imágenes</h2>
       <div className="galeria-grid">
-        {fotos.map((_, index) => (
+        {imagenes.map((_, index) => (
+          <div className="galeria-item" key={index}></div>
+        ))}
+      </div>
+
+      <h2 className="galeria-subtitulo">Videos</h2>
+      <div className="galeria-grid">
+        {videos.map((_, index) => (
           <div className="galeria-item" key={index}></div>
         ))}
       </div>
