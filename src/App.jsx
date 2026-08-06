958d71aba69f5570cd63aecdec813050005671b0
esqmarcelo <esquivelgomezmarcelo@gmail.com>
2026-08-06 15:07:04 -0300

    correcion en la funcion de cambios de seccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -9,6 +10,10 @@ import Contacto from './pages/Contacto'
 
 function AppContent() {
   const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location.pathname])
 
   return (
     <>
